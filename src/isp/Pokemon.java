c5bb5bfe837500a16d56b6eae08fd0e3b7f7224a
ybyb130 <ybyb130@inha.edu>
2023-03-18 09:49:28 +0900

    week03 02 isp before

---
+++ src/isp/Pokemon.java
@@ -0,0 +1,32 @@
+package isp;
+
+public interface Pokemon {
+    void attack();
+    void defend();
+    void fly();
+    void swim();
+
+}
+
+class Pikachu implements Pokemon{
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
