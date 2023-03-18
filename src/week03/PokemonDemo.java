a61f6c81299534ab244df376b342e78730722e7e
ybyb130 <ybyb130@inha.edu>
2023-03-18 09:38:37 +0900

    week03 01 lsp after

--- src/week03/PokemonDemo.java
+++ src/week03/PokemonDemo.java
@@ -6,20 +6,24 @@ class Pokemon {
     };
 }
 
-class FirePokemon extends Pokemon{
+class FirePokemon implements Pokemon{
     @Override
     public void attack() {
         System.out.println("불 속성 공격 성공~");
     }
 }
 
-class WaterPokemon extends Pokemon{
-}
-
-class ElectricPokemon extends Pokemon{
+class WaterPokemon implements Pokemon{
     @Override
     public void attack() {
-        throw new UnsupportedOperationException("전기 공격이 아직 완성되지 않았습니다");
+        System.out.println("물 속성 공격 성공!!");
+    }
+}
+
+class ElectricPokemon implements Pokemon{
+    @Override
+    public void attack() {
+        System.out.println("전기 속성 공격 성공");
     }
 }
 
